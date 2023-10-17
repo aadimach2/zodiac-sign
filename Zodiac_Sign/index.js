e44f9ffc0d276a45e8be2f515df846fd1fc59b69
aadimach2 <147931709+aadimach2@users.noreply.github.com>
2023-10-17 20:41:01 +0530

    Add files via upload

---
+++ Zodiac_Sign/index.js
@@ -0,0 +1,64 @@
+const zodiac=document.getElementById("zodiac");
+
+
+
+
+const body=document.body;
+
+const changeBackground=()=>{
+
+
+    
+    switch (zodiac.value) {
+        case "Aries":
+            body.style.backgroundColor= "Red";
+            break;
+
+            case "Tarus":
+            body.style.backgroundColor= "green";
+            break;
+            case "Gemini":
+            body.style.backgroundColor= "blue";
+            break;
+            case "Cancer":
+            body.style.backgroundColor= "coral";
+            break;
+            case "Leo":
+            body.style.backgroundColor= "cyan";
+            break;
+            case "Virgo":
+            body.style.backgroundColor= "brown";
+            break;
+            case "Libra":
+            body.style.backgroundColor= "pink";
+            break;
+            case "Scorpio":
+            body.style.backgroundColor= "purple";
+            break;
+            case "Sagittarius":
+            body.style.backgroundColor= "teal";
+            break;
+            case "Capricorn":
+            body.style.backgroundColor= "orange";
+            break;
+            case "Aquarius":
+            body.style.backgroundColor= "black";
+            break;
+            case "Pisces":
+            body.style.backgroundColor= "violet";
+            break;
+
+
+
+
+
+
+    
+        default:
+            body.style.backgroundColor="white";
+            break;
+    }
+
+
+
+};
